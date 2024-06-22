c10b5a30f37940d181bbc78b311e64472db2f51a
Miranda <135401173+rey03Lu@users.noreply.github.com>
2024-06-22 02:17:36 -0600

    archivos js y hmtl adicionales

--- js/script.js
+++ js/script.js
@@ -1,93 +1,245 @@
-document.addEventListener("DOMContentLoaded", function () {  
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
 
-  // JavaScript para cargar el head
-  fetch('partials/head.html')
-    .then(response => response.text())
-    .then(data => {
-      document.head.innerHTML = data;
+    // JavaScript para cargar el head
+    fetch('partials/head.html')
+      .then(response => response.text())
+      .then(data => {
+        document.head.innerHTML = data;
+      });
+  
+    // JavaScript para cargar el header
+    fetch('partials/header.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header').innerHTML = data;
+        setActiveNavLink(); // Llamamos a la función para activar el link correspondiente
+      });
+  
+    // JavaScript para cargar el footer
+    fetch('partials/footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer').innerHTML = data;
+      });
+  
+    //JavaScript para setear el link activo en el nav
+    function setActiveNavLink() {
+      const path = window.location.pathname;
+      const page = path.split("/").pop();
+      const navLinks = document.querySelectorAll('header .container nav a');
+  
+      navLinks.forEach(link => {
+        if (link.getAttribute('href') === page) {
+          link.classList.add('active');
+        }
+      });
+  
+    }
+  
+  });
+  
+  
+  /* Slider principal*/
+  let slideIndex = 0;
+  
+  /* Función para mostrar los slide */
+  function showSlides(n) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+  
+    // Normaliza el índice de las diapositivas
+    slideIndex = (n + totalSlides) % totalSlides;
+  
+    slides.forEach((slide, index) => {
+      // Oculta todas las diapositivas
+      slide.style.display = 'none';
     });
-
-  // JavaScript para cargar el header
-  fetch('partials/header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header').innerHTML = data;
-      setActiveNavLink(); // Llamamos a la función para activar el link correspondiente
-    });
-
-  // JavaScript para cargar el footer
-  fetch('partials/footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer').innerHTML = data;
-    });
-
-  //JavaScript para setear el link activo en el nav
-  function setActiveNavLink() {
-    const path = window.location.pathname;
-    const page = path.split("/").pop();
-    const navLinks = document.querySelectorAll('header .container nav a');
-
-    navLinks.forEach(link => {
-      if (link.getAttribute('href') === page) {
-        link.classList.add('active');
-      }
-    });
-
+  
+    // Muestra la diapositiva actual
+    slides[slideIndex].style.display = 'block';
   }
-
-});
-//PARA WHATSAPP
-function openWhatsApp() {
-  var url = "https://wa.me/50376200583?text=Hola!%20Quisiera%20m%C3%A1s%20informaci%C3%B3n.";
-  window.open(url, '_blank');
-}
-// Asignar el evento click al botón al cargar el DOM
-document.addEventListener("DOMContentLoaded", function() {
-  var btnWhatsApp = document.getElementById("btnWhatsApp");
-  btnWhatsApp.addEventListener("click", openWhatsApp);
-});
-
-//PARA ENVIAR LOS CORREOS
-const btn = document.getElementById('button');
-
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Enviando...';
-
-   const serviceID = 'default_service';
-   const templateID = 'template_s7z5u2l';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-        // Acción cuando el envío es exitoso
-        btn.value = 'Enviar'; // Cambiar el valor del botón si es necesario
-        showAlert('success', '¡Mensaje enviado!', 'Nos pondremos en contacto contigo pronto.');
-        // Limpiar los campos del formulario
-        document.getElementById('form').reset();
-    }, (err) => {
-        // Acción cuando hay un error
-        btn.value = 'Enviar'; // Cambiar el valor del botón si es necesario
-        showAlert('danger', 'Error al enviar mensaje', 'Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.');
-        console.error('Error al enviar el mensaje:', err); // Mostrar el error en la consola para depuración
+  
+  function plusSlides(n) {
+    showSlides(slideIndex + n);
+  }
+  
+  // Mostrar la primera diapositiva
+  document.addEventListener("DOMContentLoaded", function () {
+    showSlides(slideIndex);
+  });
+  
+  // Auto slide functionality (optional)
+  let autoSlide = setInterval(() => {
+    plusSlides(1);
+  }, 9000);
+  
+  // Pause auto slide on hover
+  document.querySelector('.slider').addEventListener('mouseover', () => {
+    clearInterval(autoSlide);
+  });
+  
+  document.querySelector('.slider').addEventListener('mouseout', () => {
+    autoSlide = setInterval(() => {
+      plusSlides(1);
+    }, 9000);
+  });
+  
+  
+  // Slider para "Nosotros"
+  let slideIndexNosotros = 0;
+  let slidesNosotros = document.getElementsByClassName("slide-nosotros");
+  let indicators = document.getElementsByClassName("indicator");
+  
+  function showSlidesNosotros() {
+    for (let i = 0; i < slidesNosotros.length; i++) {
+      slidesNosotros[i].style.display = "none";
+    }
+    slideIndexNosotros++;
+    if (slideIndexNosotros > slidesNosotros.length) { slideIndexNosotros = 1 }
+    for (let i = 0; i < indicators.length; i++) {
+      indicators[i].className = indicators[i].className.replace(" active-us", "");
+    }
+    slidesNosotros[slideIndexNosotros - 1].style.display = "block";
+    indicators[slideIndexNosotros - 1].className += " active-us";
+    setTimeout(showSlidesNosotros, 6000); // Cambia la imagen cada 4 segundos
+  }
+  
+  function currentSlideNosotros(n) {
+    slideIndexNosotros = n;
+    showSlidesNosotros();
+  }
+  
+  showSlidesNosotros();
+  
+  
+  
+  // Filtrado de proyectos
+  function filterSelection(category) {
+    const galleryItems = document.querySelectorAll('.galeria-item');
+  
+    if (category === 'all') {
+      galleryItems.forEach(item => item.classList.add('show'));
+    } else {
+      galleryItems.forEach(item => {
+        item.classList.remove('show');
+        if (item.classList.contains(category)) {
+          item.classList.add('show');
+        }
+      });
+    }
+  
+    // Rearreglar los elementos para que se distribuyan en filas de tres
+    const visibleItems = Array.from(galleryItems).filter(item => item.classList.contains('show'));
+    galleryItems.forEach(item => item.style.order = '');
+  
+    visibleItems.forEach((item, index) => {
+      item.style.order = Math.floor(index / 3);
     });
-
-// Función para mostrar alertas utilizando Bootstrap
-function showAlert(type, title, message) {
-    const alertMarkup = `
-        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
-            <strong>${title}</strong> ${message}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    `;
-    // Agregar la alerta al DOM
-    document.getElementById('alert-container').innerHTML = alertMarkup;
-
-    // Cerrar automáticamente la alerta después de 5 segundos
-    setTimeout(function() {
-        document.querySelector('.alert').remove();
-    }, 5000);
-}
-});
+  
+    // Actualizar el botón activo
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    filterButtons.forEach(button => button.classList.remove('active-projects'));
+  
+    const activeButton = document.querySelector(`.filter-btn[onclick="filterSelection('${category}')"]`);
+    if (activeButton) {
+      activeButton.classList.add('active-projects');
+    }
+  }
+  
+  // Activar el filtro "all" por defecto
+  document.addEventListener('DOMContentLoaded', function () {
+    filterSelection('all');
+  });
+  
+  
+  
+  // Carrusel para "Clientes"
+  document.addEventListener('DOMContentLoaded', function () {
+    const carruselTrack = document.querySelector('.carrusel-track');
+    const items = document.querySelectorAll('.carrusel-item');
+    const totalItems = items.length / 2; // Only count the unique items
+    let currentIndex = 0;
+  
+    function showNextSlide() {
+      currentIndex++;
+      if (currentIndex >= totalItems) {
+        carruselTrack.style.transition = 'none';
+        carruselTrack.style.transform = 'translateX(0)';
+        currentIndex = 0;
+        setTimeout(() => {
+          carruselTrack.style.transition = 'transform 0.5s ease-in-out';
+          showNextSlide();
+        }, 20); // Small delay to reset transition
+      } else {
+        updateCarrusel();
+      }
+    }
+  
+    function updateCarrusel() {
+      const offset = -currentIndex * 25; // 25% for each item width
+      carruselTrack.style.transform = `translateX(${offset}%)`;
+    }
+  
+    setInterval(showNextSlide, 2000); // Cambia de imagen cada 2 segundos
+  });
+  
+  
+  
+  
+  //PARA WHATSAPP
+  function openWhatsApp() {
+    var url = "https://wa.me/50376200583?text=Hola!%20Quisiera%20m%C3%A1s%20informaci%C3%B3n.";
+    window.open(url, '_blank');
+  }
+  // Asignar el evento click al botón al cargar el DOM
+  document.addEventListener("DOMContentLoaded", function() {
+    var btnWhatsApp = document.getElementById("btnWhatsApp");
+    btnWhatsApp.addEventListener("click", openWhatsApp);
+  });
+  
+  //PARA ENVIAR LOS CORREOS
+  const btn = document.getElementById('button');
+  
+  document.getElementById('form')
+   .addEventListener('submit', function(event) {
+     event.preventDefault();
+  
+     btn.value = 'Enviando...';
+  
+     const serviceID = 'default_service';
+     const templateID = 'template_s7z5u2l';
+  
+     emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+          // Acción cuando el envío es exitoso
+          btn.value = 'Enviar'; // Cambiar el valor del botón si es necesario
+          showAlert('success', '¡Mensaje enviado!', 'Nos pondremos en contacto contigo pronto.');
+          // Limpiar los campos del formulario
+          document.getElementById('form').reset();
+      }, (err) => {
+          // Acción cuando hay un error
+          btn.value = 'Enviar'; // Cambiar el valor del botón si es necesario
+          showAlert('danger', 'Error al enviar mensaje', 'Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.');
+          console.error('Error al enviar el mensaje:', err); // Mostrar el error en la consola para depuración
+      });
+  
+  // Función para mostrar alertas utilizando Bootstrap
+  function showAlert(type, title, message) {
+      const alertMarkup = `
+          <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+              <strong>${title}</strong> ${message}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+      `;
+      // Agregar la alerta al DOM
+      document.getElementById('alert-container').innerHTML = alertMarkup;
+  
+      // Cerrar automáticamente la alerta después de 5 segundos
+      setTimeout(function() {
+          document.querySelector('.alert').remove();
+      }, 5000);
+  }
+  
+  });
+  
